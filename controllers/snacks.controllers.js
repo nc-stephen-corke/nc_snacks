a8bbf6945d22f28ac1ed05f794d5771eb060402c
nc_stephen_corke <stephen.corke@northcoders.com>
2024-11-15 15:25:36 +0000

    start

--- controllers/snacks.controllers.js
+++ controllers/snacks.controllers.js
@@ -1,23 +1,26 @@
-const {fetchSnacks, fetchSnackBySnackId, addSnack} = require("../models/snacks.models");
+const {
+  fetchSnacks,
+  fetchSnackBySnackId,
+  addSnack,
+} = require('../models/snacks.models');
 
-const getSnacks = (request, response) => { 
-    fetchSnacks().then((snacks) => { 
-     response.status(200).send({snacks})
-    })
-}
-const getSnackBySnackId = (request, response) => { 
-    const { snack_id } = request.params
-    fetchSnackBySnackId(snack_id).then((snack) => { 
-        response.status(200).send({snack: snack})
-    })
+const getSnacks = (request, response) => {
+  fetchSnacks().then((snacks) => {
+    response.status(200).send({ snacks });
+  });
+};
+const getSnackBySnackId = (request, response) => {
+  const { snack_id } = request.params;
+  fetchSnackBySnackId(snack_id).then((snack) => {
+    response.status(200).send({ snack: snack });
+  });
 };
 
 const postSnack = (request, response) => {
-    const newSnack = request.body;
-    addSnack(newSnack).then(() => {
-        response.status(201).send({ "snack added": newSnack });
-    })
-   
-}
+  const newSnack = request.body;
+  addSnack(newSnack).then(() => {
+    response.status(201).send({ 'snack added': newSnack });
+  });
+};
 
-module.exports = {getSnacks, getSnackBySnackId, postSnack}
+module.exports = { getSnacks, getSnackBySnackId, postSnack };
